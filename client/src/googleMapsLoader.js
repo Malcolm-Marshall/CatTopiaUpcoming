78a7d4108c2c84b9632b5d9b75d94a3d40a8920b
Malcolm-Marshall <73361300+Malcolm-Marshall@users.noreply.github.com>
2026-03-13 21:56:30 -0600

    add client start script for railway

--- client/src/googleMapsLoader.js
+++ client/src/googleMapsLoader.js
@@ -4,7 +4,12 @@ export function googleMapsLoader() {
   if (loadingPromise) return loadingPromise;
 
   const apiKey = import.meta.env.VITE_MAPS_JS_API_KEY;
+  const API_URL = import.meta.env.VITE_API_URL;
   if (!apiKey) throw new Error("Missing VITE_MAPS_JS_API_KEY");
+
+  fetch(`${API_URL}/api/health`)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
   // Already loaded?
   if (window.google?.maps) return Promise.resolve(window.google.maps);
